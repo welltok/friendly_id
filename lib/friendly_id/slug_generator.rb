921d453429f1cacefc4d7d0c3c6d6a9ac7bf09b7
Justin Smestad <justin.smestad@gmail.com>
2012-08-23 11:24:28 -0600

    check for sqlserver otherwise use LENGTH

--- lib/friendly_id/slug_generator.rb
+++ lib/friendly_id/slug_generator.rb
@@ -59,8 +59,11 @@ module FriendlyId
       base << "ESCAPE '\\'" if sluggable.connection.adapter_name =~ /sqlite/i
       scope = sluggable_class.unscoped.where(base, normalized, wildcard)
       scope = scope.where("#{pkey} <> ?", value) unless sluggable.new_record?
-      # scope = scope.order("LENGTH(#{column}) DESC, #{column} DESC")
-      scope = scope.order("LEN(#{column}) DESC, #{column} DESC")
+      scope = if sluggable.connection.adapter_name =~ /sqlserver/i
+                scope.order("LEN(#{column}) DESC, #{column} DESC")
+              else
+                scope.order("LENGTH(#{column}) DESC, #{column} DESC")
+              end
     end
 
     def friendly_id_config
